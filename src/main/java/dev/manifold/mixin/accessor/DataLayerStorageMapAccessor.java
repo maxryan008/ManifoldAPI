f8c064c991f0382163e9b9aebc01119473f4bf2f
Roelymole <roelymole@gmail.com>
2025-07-23 01:01:05 +0100

    Fix mixin target warning in DataLayerStorageMapAccessor

--- src/main/java/dev/manifold/mixin/accessor/DataLayerStorageMapAccessor.java
+++ src/main/java/dev/manifold/mixin/accessor/DataLayerStorageMapAccessor.java
@@ -2,10 +2,11 @@ package dev.manifold.mixin.accessor;
 
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import net.minecraft.world.level.chunk.DataLayer;
+import net.minecraft.world.level.lighting.DataLayerStorageMap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(targets = "net.minecraft.world.level.lighting.DataLayerStorageMap") // interface
+@Mixin(DataLayerStorageMap.class) // interface
 public interface DataLayerStorageMapAccessor {
     @Accessor("map")
     Long2ObjectOpenHashMap<DataLayer> manifold$getMap();
